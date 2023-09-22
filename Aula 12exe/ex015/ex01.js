cc9b39fee75eb15bc132545531eab642315337df
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2023-09-21 21:09:31 -0300

    atualização no css

--- Aula 12exe/ex015/ex01.js
+++ Aula 12exe/ex015/ex01.js
@@ -44,6 +44,7 @@ function verificar(){
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 };
 
