78d21ef8a5b5749ec406682db30f8196a5f09ef2
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2023-09-27 17:12:09 -0300

    atualização do exercício

---
+++ Aula13/ambiente.js
@@ -0,0 +1,24 @@
+//let fine = 0
+/*while(fine <5){
+    console.log('Tudo bem?')
+    fine++
+};*/
+
+/*for(i = 0; i < 2; i ++){
+    console.log('Tudo bem?')
+};*/
+
+/*let contador = 1
+do{
+    console.log(contador)
+    contador++
+}while(contador <= 5);*/
+
+let numbers= [7, 22, 13];
+let n = [];
+
+for(l = 0; l < numbers.length; l++){
+    n.push(numbers[l]);
+}
+
+console.log(n);
---
+++ Aula14/ex001.js
@@ -0,0 +1,4 @@
+for(c = 1; c <= 15; c+=2){
+    console.log(c)
+}
+console.log('FIM!')
