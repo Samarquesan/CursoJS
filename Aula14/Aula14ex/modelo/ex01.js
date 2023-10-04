7d099b05ae8c27b35280562f2cf3dacbd403a9ce
Samuel Marques dos Santos <78937543+Samarquesan@users.noreply.github.com>
2023-10-03 23:43:47 -0300

    Criação do script tabuada

--- Aula14/Aula14ex/modelo/ex01.js
+++ Aula14/Aula14ex/modelo/ex01.js
@@ -1,2 +1,19 @@
 
-alert('Olá!')
+function tabuada(){
+    let startNumber = document.getElementById('idnumber')
+    let ressult = document.getElementById('idresp')
+    let tab = document.getElementById('seltab')
+    if(startNumber.value.length == 0){
+        window.alert('Por favor, digite um número!')
+    } else{
+        let n = Number(startNumber.value)
+       tab.innerHTML = ''
+       for(c = 1; c <= 10; c++){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n * c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+    
+}
